451814b5944db2756836a5020d58ee858d804b55
kaikei-e <e_kaikei@protonmail.com>
2021-04-18 17:13:04 +0900

    Making UI.

--- main.go
+++ main.go
@@ -6,20 +6,22 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+
 	//"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/widget"
 	//"fyne.io/fyne/v2/layout"
 	//"fyne.io/fyne/v2/dialog"
 
+	"sortinGopher/imagesClassifier"
 	"sortinGopher/permanentPath"
 	"sortinGopher/unzipper"
-	"sortinGopher/imagesClassifier"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("SortinGopher")
-	w.Resize(fyne.NewSize(900, 600))
+	w.Resize(fyne.NewSize(750, 450))
 
 	input  := widget.NewEntry()
 	input.SetPlaceHolder("Please enter the path where the zip file is located ... ")
@@ -36,11 +38,16 @@ func main() {
 
 	})
 
+	w.SetContent(fyne.NewContainerWithLayout(
+		layout.NewVBoxLayout(),
+		content,
+		layout.NewSpacer(),
+		execButton,
+	),
+
+)
 
 
-
-
-	w.SetContent(content)
 	w.ShowAndRun()
 }
 
