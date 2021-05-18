83ab437253c27ab0fab99ff3bdd7aaf3f7a8602f
kaikei <kaikei-e@protonmail.com>
2021-05-18 22:07:07 +0900

    Testing improvements of file classifing.

--- imagesClassifier/classifier.go
+++ imagesClassifier/classifier.go
@@ -118,12 +118,12 @@ func imgClassifier(paths []string, folderPath string){
 				panic(err)
 				}
 
-				if err := os.Rename(imagePath, folderPath + "/" + fileName + "/" + fileName + tFormatted + "No" + strconv.Itoa(i) + filenameExtension)
+				if err := os.Rename(imagePath, folderPath + "/" + fileName + "/" + fileName + "-"+ tFormatted + "No" + strconv.Itoa(i) + filenameExtension)
 				err != nil{
 					panic(err)
 				}
 			}else{
-				if err := os.Rename(imagePath, folderPath + "/" + fileName + "/" + fileName + tFormatted + "No" + strconv.Itoa(i) + filenameExtension)
+				if err := os.Rename(imagePath, folderPath + "/" + fileName + "/" + fileName + "+" + tFormatted + "No" + strconv.Itoa(i) + filenameExtension)
 				err != nil{
 					panic(err)
 				}
