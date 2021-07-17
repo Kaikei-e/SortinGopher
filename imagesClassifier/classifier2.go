1df138a73727b69e59a1bfa21cab68b8a7b7768b
kaikei <kaikei-e@protonmail.com>
2021-07-18 03:24:31 +0900

    Update: rebuild exe

--- imagesClassifier/classifier2.go
+++ imagesClassifier/classifier2.go
@@ -31,9 +31,6 @@ func ImgClassifier(dirName string, wg *sync.WaitGroup) {
 		if f.IsDir(){
 			continue
 		}
-		log.Println(f.Name())
-		log.Println(i)
-
 		fileName := filepath.Base(dirName + "/" + f.Name())
 
 
@@ -79,7 +76,7 @@ func ImgClassifier(dirName string, wg *sync.WaitGroup) {
 					err != nil{
 						panic(err)
 				}
-				
+
 		}
 
 
